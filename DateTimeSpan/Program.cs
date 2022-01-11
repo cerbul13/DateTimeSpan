9c6b4b34288d41cd4a10fb79928fc064bfbe5082
cerbul13 <cerbadan@yahoo.com>
2022-01-11 12:09:55 +0200

    working

--- DateTimeSpan/Program.cs
+++ DateTimeSpan/Program.cs
@@ -9,8 +9,7 @@ namespace DateTimeSpan
             DateTime date1 = new DateTime(2020, 10, 25);
             DateTime date2 = new DateTime(2022, 10, 26);
             TimeSpan span1 = date2 - date1;
-            //TimeSpan span2 = new TimeSpan(1, 0, 0, 0);
-            DateTime difference = DateTime.MinValue + span1;// - span2;
+            DateTime difference = DateTime.MinValue + span1;
             Console.WriteLine($"Years: {difference.Year-1}, Months: {difference.Month-1}, Days: {difference.Day-1}.");
         }
     }
