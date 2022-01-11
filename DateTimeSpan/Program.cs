1aad6407e6fe108ad3b710d04eec559fceb4746d
cerbul13 <cerbadan@yahoo.com>
2022-01-11 12:00:50 +0200

    second commit

--- DateTimeSpan/Program.cs
+++ DateTimeSpan/Program.cs
@@ -10,6 +10,7 @@ namespace DateTimeSpan
             DateTime date2 = new DateTime(2022, 10, 26);
             TimeSpan span1 = date2 - date1;
             //TimeSpan span2 = new TimeSpan(1, 0, 0, 0);
+            //test repo for branches
             DateTime difference = DateTime.MinValue + span1;// - span2;
             Console.WriteLine($"Years: {difference.Year-1}, Months: {difference.Month-1}, Days: {difference.Day-1}.");
         }
